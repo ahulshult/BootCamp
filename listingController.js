0458a8d65afe58af7f781db0fa2692cf05c64425
ahulshult <ahulshult@ufl.edu>
2017-09-02 20:55:45 -0400

    Not working

--- listingController.js
+++ listingController.js
@@ -1,14 +1,21 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    $scope.name = "";
+    $scope.code = "";
+    $scope.text = ["345"];
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+
+    $scope.addListing = function() {
+    if ($scope.name !== "" && $scope.code !== ""){
+      $scope.text.push($scope.name);
+      $scope.text.push($scope.code);
+      }
+    }
+    $scope.deleteListing = function(index) {}
   }
 ]);
