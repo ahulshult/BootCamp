e7d8161159a3b6dbc9106185a4ba846fa657991a
ahulshult <ahulshult@ufl.edu>
2017-09-04 17:16:12 -0400

    delete function and sort function works

--- listingController.js
+++ listingController.js
@@ -2,7 +2,7 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-    $scope.searchText = "d";
+    $scope.searchText = "";
     //console.log($scope.searchText);
     $scope.sortType = "code";
     /*
@@ -14,17 +14,28 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
         var myForm = document.forms[0];
         for(i=0; i<5; i++){
           arr[i] = myForm.elements[i].value;
-          console.log(arr);
         }
       if (arr[0] !== "" && arr[1] !== ""){
         $scope.listings.push({'code': arr[0], 'name':arr[1], 'coordinates': {'latitude': arr[2], 'longitude': arr[3]}, 'address': arr[4]});
-        console.log($scope.listings);
       }
       else{
-        $scope.searchItem = "";
+        alert( "Please make sure 'Code' and 'Name' fields are complete." );
       }
     };
-    $scope.deleteListing = function(index) {};
+    $scope.deleteListing = function(code) {
+      var bool = -1;
+		  var getArr = eval( $scope.listings );
+		  for( var i = 0; i < getArr.length; i++ ) {
+           if( getArr[i].code === code ) {
+				         bool = i;
+				         break;
+			           }
+  		}
+  		if( bool === -1 ) {
+  			alert( "Cannot Delete" );
+  		}
+  		$scope.listings.splice( bool, 1 );
+      };
     $scope.showDetails = function(index) {};
   }
 ]);
