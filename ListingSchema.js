47bbf30848ca5e27d25b391b8c50de51380d4637
ahulshult <ahulshult@ufl.edu>
2017-09-05 09:12:32 -0400

    I think it works

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,14 +1,29 @@
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
   /* your code here */
+  name: String;
+  code: String;
+  meta: {
+    coordinates: {
+      latitude: Number;
+      longitude: Number;
+    }
+    address: String;
+  }
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
+  var currentDate = new Date();
+  this.updated_at = currentDate;
+  if(!this.created_at){
+    this.created_at=currentDate;
+  }
+  next();
   /* your code here */
 });
 
