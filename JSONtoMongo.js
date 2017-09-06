a4d74b19ee544c2f3c53b9115da4a38aa9b48001
ahulshult <ahulshult@ufl.edu>
2017-09-06 13:21:53 -0400

    works

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -8,10 +8,59 @@ var fs = require('fs'),
     Schema = mongoose.Schema,
     Listing = require('./ListingSchema.js'),
     config = require('./config');
-    console.log(config.uri)
+    //json = require('./listings.json');
 
+var listingData = undefined;
 /* Connect to your database */
+//mongoose.connect(config.db.uri);
+  //done();
 
+fs.readFile('./listings.json', 'utf8', function(err, data){
+  if (err) throw err;
+  listingData = JSON.parse(data);
+  openDB();
+  enterData();
+  mongoose.connection.close();
+  });
+
+function openDB(){
+mongoose.connect(config.db.uri);
+}
+
+function enterData(){
+for(var i=0; i<listingData.entries.length; i++){
+    //console.log(listingData.entries[i]);
+    var entry = listingData.entries[i];
+    var newEntry = new Listing({
+      code: entry.code,
+      name: entry.name,
+      latitude: entry.latitude,
+      longitude: entry.longitude,
+      address: entry.address
+    });
+    newEntry.save(function(err){
+      if (err) throw err;
+    });
+  }
+}
+
+
+/*
+for(var i=0; i<listingData.entries.length; i++){
+  entry = listingData[i];
+  var newEntry = new Listing({
+    code: entry.code,
+    name: entry.name,
+    latitude: entry.latitude,
+    longitude: entry.longitude,
+    address: entry.address
+  });
+  newEntry.save(function(err){
+    if (err) throw err;
+  });
+
+}
+//done();
 /*
   Instantiate a mongoose model for each listing object in the JSON file,
   and then save it to your Mongo database
